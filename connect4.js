55917bd2778e0b89f2d73f5455fe9aade560ed08
Michael Aberasturi <aberasturi.michael@gmail.com>
2023-09-30 11:07:20 -0700

    Connect Four OO

---
+++ connect4.js
@@ -0,0 +1,156 @@
+"use strict";
+
+class Player {
+  constructor(color) {
+    this.color = color;
+  }
+}
+
+class Game {
+  constructor(player1, player2, height = 6, width = 7) {
+    this.player1 = player1;
+    this.player2 = player2;
+    this.currPlayer = player1;
+    this.width = width;
+    this.height = height;
+    this.handleClick = this.handleClick.bind(this);
+    this.makeBoard();
+    this.makeHtmlBoard();
+  }
+
+  makeBoard() {
+    this.board = [];
+    for (let y = 0; y < this.height; y++) {
+      this.board.push(Array.from({ length: this.width }));
+    }
+  }
+
+  makeHtmlBoard() {
+    const board = document.getElementById("board");
+    board.innerHTML = "";
+    const top = document.createElement("tr");
+    top.setAttribute("id", "column-top");
+
+    top.addEventListener("click", this.handleClick);
+
+    for (let x = 0; x < this.width; x++) {
+      const headCell = document.createElement("td");
+      headCell.setAttribute("id", x);
+      top.append(headCell);
+    }
+
+    board.append(top);
+
+    for (let y = 0; y < this.height; y++) {
+      const row = document.createElement("tr");
+
+      for (let x = 0; x < this.width; x++) {
+        const cell = document.createElement("td");
+        cell.setAttribute("id", `${y}-${x}`);
+        row.append(cell);
+      }
+
+      board.append(row);
+    }
+  }
+
+  findSpotForCol(x) {
+    for (let y = this.height - 1; y >= 0; y--) {
+      if (!this.board[y][x]) {
+        return y;
+      }
+    }
+    return null;
+  }
+
+  placeInTable(y, x) {
+    const piece = document.createElement("div");
+    piece.classList.add("piece");
+    piece.style.backgroundColor = this.currPlayer.color;
+    piece.style.top = -50 * (y + 2);
+
+    const spot = document.getElementById(`${y}-${x}`);
+    spot.append(piece);
+  }
+
+  endGame(msg) {
+    alert(msg);
+    const top = document.getElementById("column-top");
+    top.removeEventListener("click", this.handleClick);
+  }
+
+  handleClick(evt) {
+    const x = +evt.target.id;
+
+    const y = this.findSpotForCol(x);
+    if (y === null) {
+      return;
+    }
+
+    this.board[y][x] = this.currPlayer;
+    this.placeInTable(y, x);
+
+    if (this.checkForWin()) {
+      return this.endGame(`${this.currPlayer} won!`);
+    }
+
+    if (this.board.every((row) => row.every((cell) => cell))) {
+      return this.endGame("Tie!");
+    }
+
+    this.currPlayer =
+      this.currPlayer === this.player1 ? this.player2 : this.player1;
+  }
+
+  checkForWin() {
+    const _win = (cells) =>
+      cells.every(
+        ([y, x]) =>
+          y >= 0 &&
+          y < this.height &&
+          x >= 0 &&
+          x < this.width &&
+          this.board[y][x] === this.currPlayer
+      );
+
+    for (let y = 0; y < this.height; y++) {
+      for (let x = 0; x < this.width; x++) {
+        const horiz = [
+          [y, x],
+          [y, x + 1],
+          [y, x + 2],
+          [y, x + 3],
+        ];
+        const vert = [
+          [y, x],
+          [y + 1, x],
+          [y + 2, x],
+          [y + 3, x],
+        ];
+        const diagDR = [
+          [y, x],
+          [y + 1, x + 1],
+          [y + 2, x + 2],
+          [y + 3, x + 3],
+        ];
+        const diagDL = [
+          [y, x],
+          [y + 1, x - 1],
+          [y + 2, x - 2],
+          [y + 3, x - 3],
+        ];
+
+        if (_win(horiz) || _win(vert) || _win(diagDR) || _win(diagDL)) {
+          return true;
+        }
+      }
+    }
+  }
+}
+
+const startButton = document.getElementById("btn");
+startButton.addEventListener("click", function () {
+  let player1 = new Player(document.getElementById("player1").value);
+  let player2 = new Player(document.getElementById("player2").value);
+  new Game(player1, player2);
+});
